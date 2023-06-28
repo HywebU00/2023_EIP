96a58c73ca154dff4f2eac763aeca8c3c56755bd
Tseng <tseng7510@gmail.com>
2023-06-28 09:21:37 +0800

    更新

---
+++ include/Menu.js
@@ -0,0 +1,75 @@
+const content = `
+        <button class="sideBtn"></button>
+        <div class="sideMenu">
+          <div class="logo">
+            <div class="img"><img src="./images/logo.png" alt="" /></div>
+            <span><img src="./images/logo_font.png" alt="" /></span>
+          </div>
+          <nav>
+            <ul>
+              <li>
+                <a href="mp.html"><span class="pageTitle">儀表板</span></a>
+              </li>
+              <li>
+                <a href="#"><span>線上申請</span></a>
+                <ul>
+                  <li><a href="#">請領物品</a></li>
+                  <li><a href="#">遠端連線申請</a></li>
+                  <li><a href="#">網頁上架申請</a></li>
+                </ul>
+              </li>
+              <li>
+                <a href="#"><span>資源借用</span></a>
+                <ul>
+                  <li><a href="#">會議室預約</a></li>
+                  <li><a href="#">派車</a></li>
+                </ul>
+              </li>
+              <li>
+                <a href="#"><span>文件管理</span></a>
+                <ul>
+                  <li><a href="#">知識文件</a></li>
+                  <li><a href="#">常用表單下載</a></li>
+                </ul>
+              </li>
+              <li>
+                <a href="#"><span>檔案分享</span></a>
+              </li>
+              <li>
+                <a href="#"><span>應用系統</span></a>
+                <ul>
+                  <li><a href="#">差勤系統</a></li>
+                  <li><a href="#">公文系統</a></li>
+                  <li><a href="#">經費結報系統</a></li>
+                  <li><a href="#">智慧治理系統</a></li>
+                  <li><a href="project.html">案件管理</a></li>
+                  <li><a href="#">災防系統</a></li>
+                  <li><a href="#">新聞系統</a></li>
+                </ul>
+              </li>
+              <li>
+                <a href="album.html"><span>線上相簿</span></a>
+              </li>
+              <li>
+                <a href="#"><span>投票/問券調查/報名</span></a>
+              </li>
+              <li>
+                <a href="#"><span>個人資訊</span></a>
+                <ul>
+                  <li><a href="#">差勤系統</a></li>
+                  <li><a href="#">公文系統</a></li>
+                </ul>
+              </li>
+            </ul>
+          </nav>
+          <div class="account">
+            <div class="pic"><img src="./images/account.jpg" alt="" /></div>
+            <div class="infoBox">
+              <div class="name">Elle Wang</div>
+              <div class="department">企劃處</div>
+            </div>
+            <button class="logout"></button>
+          </div>
+        </div>`;
+
+document.write(content);
